343dea23175ed9227d36f3fa66506f207763df49
TruongNoDame <20522081@gm.uit.edu.vn>
2023-04-08 19:41:42 +0700

    Sort words

--- CRAFT-pytorch/my_CRAFT.py
+++ CRAFT-pytorch/my_CRAFT.py
@@ -27,24 +27,68 @@ from collections import OrderedDict
 # def str2bool(v):
 #     return v.lower() in ("yes", "y", "true", "t", "1")
 
+def sort_words(data, words, y_threshold):
+    data = np.array(data)
+    # Sắp xếp các hộp chứa theo giá trị tọa độ y
+    sorted_data = data[data[:, 1].argsort()]
+
+    # Khởi tạo danh sách để lưu trữ kết quả
+    result = []
+
+    # Duyệt qua các hộp chứa đã sắp xếp
+    current_row = []
+    for i in range(sorted_data.shape[0]):
+        # Nếu hộp chứa hiện tại thuộc cùng hàng với hộp chứa trước đó
+        if i > 0 and abs(sorted_data[i, 1] - sorted_data[i-1, 1]) <= y_threshold:
+            current_row.append(sorted_data[i])
+        else:
+            # Nếu current_row không rỗng
+            if current_row:
+                # Tính toán giá trị trung bình của các giá trị y trong current_row
+                mean_y = np.mean(current_row, axis=0)[1]
+                # Gán giá trị trung bình cho tất cả các hộp chứa trong current_row
+                current_row = np.array(current_row)
+                current_row[:, 1] = mean_y
+                # Sắp xếp các hộp chứa trong current_row theo giá trị tọa độ x
+                current_row = current_row[current_row[:, 0].argsort()]
+                result.append(current_row)
+            current_row = [sorted_data[i]]
+
+    # Thêm hàng cuối cùng vào kết quả
+    if current_row:
+        mean_y = np.mean(current_row, axis=0)[1]
+        current_row = np.array(current_row)
+        current_row[:, 1] = mean_y
+        current_row = current_row[current_row[:, 0].argsort()]
+        result.append(current_row)
+
+    new_sx = []
+    new_words = []
+    for i in result:
+        for j in i:
+            new_sx.append(j)
+    for i in new_sx:
+        new_words.append(words[i[2]])
+    return new_words, result
+
 def crop(pts, image):
 
-  """
-  Takes inputs as 8 points
-  and Returns cropped, masked image with a white background
-  """
-  rect = cv2.boundingRect(pts)
-  x,y,w,h = rect
-  cropped = image[y:y+h, x:x+w].copy()
-  pts = pts - pts.min(axis=0)
-  mask = np.zeros(cropped.shape[:2], np.uint8)
-  cv2.drawContours(mask, [pts.astype(int)], -1, (255, 255, 255), -1, cv2.LINE_AA)
-  dst = cv2.bitwise_and(cropped, cropped, mask=mask)
-  bg = np.ones_like(cropped, np.uint8)*255
-  cv2.bitwise_not(bg,bg, mask=mask)
-  dst2 = bg + dst
+    """
+    Takes inputs as 8 points
+    and Returns cropped, masked image with a white background
+    """
+    rect = cv2.boundingRect(pts)
+    x,y,w,h = rect
+    cropped = image[y:y+h, x:x+w].copy()
+    pts = pts - pts.min(axis=0)
+    mask = np.zeros(cropped.shape[:2], np.uint8)
+    cv2.drawContours(mask, [pts.astype(int)], -1, (255, 255, 255), -1, cv2.LINE_AA)
+    dst = cv2.bitwise_and(cropped, cropped, mask=mask)
+    bg = np.ones_like(cropped, np.uint8)*255
+    cv2.bitwise_not(bg,bg, mask=mask)
+    dst2 = bg + dst
 
-  return dst2
+    return dst2, [x, y]
 
 # parser = argparse.ArgumentParser(description='CRAFT Text Detection')
 # parser.add_argument('--trained_model', default='weights/craft_mlt_25k.pth', type=str, help='pretrained model')
@@ -117,6 +161,7 @@ class My_CRAFT():
     def detect(self, image_path):
         # list image cropped to show
         word_images = []
+        sx = []
 
         t = time.time()
 
@@ -127,11 +172,15 @@ class My_CRAFT():
         bboxes, polys, score_text = test.test_net(self.net, image, self.text_threshold, self.link_threshold,
             self.low_text, self.cuda, self.poly, self.canvas_size, self.mag_ratio, self.refine_net)
 
+        dem = 0
         for box_num in range(len(bboxes)):
             pts = bboxes[box_num]
             if np.all(pts) > 0:
-                word = crop(pts, image)
+                word, idx = crop(pts, image)
+                idx.append(dem)
+                sx.append(idx)
                 word_images.append(word)
+                dem += 1
 
         # save score text
         filename, file_ext = os.path.splitext(os.path.basename(image_path))
@@ -141,8 +190,8 @@ class My_CRAFT():
         file_utils.saveResult(image_path, image[:,:,::-1], polys, dirname=result_folder)
 
         print("elapsed time : {}s".format(time.time() - t))
-
-        return word_images
+        words, re = sort_words(sx, word_images, 15)
+        return words
 
 if __name__ == '__main__':
     img_path = "image/cc.png"
@@ -151,7 +200,8 @@ if __name__ == '__main__':
     # list image cropped to show
     model = My_CRAFT(weight_path)
     word_images = model.detect(img_path)
-    for word in word_images:
-        cv2.imshow("word", word)
-        cv2.waitKey(0)
-        cv2.destroyAllWindows()
+    # print(len(word_images))
+    word = word_images[3]
+    cv2.imshow("word", word)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
